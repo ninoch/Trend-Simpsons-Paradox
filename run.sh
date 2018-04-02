b186b9215ea020715a295a36f23a185d55d28767
Nazanin AlipourFard <nazaninalipourfard@guest-wireless-upc-1605-10-120-089-071.usc.edu>
2018-04-02 15:24:52 -0700

    print info for run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# make the model directory if it doesn't exist already
+
+printf "\n *** BASH: Making folders for outputs\n"
+
+mkdir -p temporary_files
+mkdir -p temporary_files/$csv_file_name
+mkdir -p output
+mkdir -p store_results
+
 ###############################
 #       BINS PARAMETERS       #
 ###############################
+
 num_of_bins=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['num_of_bins']")
 least_num_of_datapoints=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['least_num_of_datapoints_in_each_bin']")
 target_variable_column=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['target_variable_column']")
@@ -12,23 +22,19 @@ csv_file_name=$(cat "input_info.json" | python -c "import sys, json; print json.
 #       FINDING BINS       #
 ############################
 
-# make the model directory if it doesn't exist already
-mkdir -p temporary_files
-mkdir -p temporary_files/$csv_file_name
+printf "\n *** BASH: Run the binning algorithm and store results in temporary_files/ \n"
 
 cd scripts
 make
 ./cppexe -infile:../input/$csv_file_name".csv" -outfolder:../temporary_files/$csv_file_name -ycol:$target_variable_column -nbins:$num_of_bins -mindatapoints:$least_num_of_datapoints
 
-#####################################
-#       REGRESSION PARAMETERS       #
-#####################################
+######################################
+#       FINDING SIMPSONS PAIRS       #
+######################################
 
-mkdir -p ../output
-mkdir -p ../store_results
+printf "\n *** BASH: Run Trend Simpson's Paradox algorithm to find pairs and store results in output/ \n"
 
 python trend_simpson.py
 
-
-printf "\nDONE\n"
+printf "\n *** BASH: DONE\n"
 
