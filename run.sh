6a4d510348643b8ebdd6ae5fd70a86d258607255
Eva <evaspam@arcor.de>
2022-02-19 23:47:38 +0100

    Fix print statement for json.load

--- run.sh
+++ run.sh
@@ -4,11 +4,10 @@
 #       BINS PARAMETERS       #
 ###############################
 
-num_of_bins=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['num_of_bins']")
-least_num_of_datapoints=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['least_num_of_datapoints_in_each_bin']")
-target_variable_column=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['target_variable_column']")
-csv_file_name=$(cat "input_info.json" | python -c "import sys, json; print json.load(sys.stdin)['csv_file_name']")
-
+num_of_bins=$(cat "input_info.json" | python -c "import sys, json; print(json.load(sys.stdin)['num_of_bins'])")
+least_num_of_datapoints=$(cat "input_info.json" | python -c "import sys, json; print(json.load(sys.stdin)['least_num_of_datapoints_in_each_bin'])")
+target_variable_column=$(cat "input_info.json" | python -c "import sys, json; print(json.load(sys.stdin)['target_variable_column'])")
+csv_file_name=$(cat "input_info.json" | python -c "import sys, json; print(json.load(sys.stdin)['csv_file_name'])")
 
 ##################################
 #       MAKING OUT FOLDERS       #
